30f5e4c4d4616053dc27e24d3346c55123751cbd
GustavoHenriqueSan <132524846+GustavoHenriqueSan@users.noreply.github.com>
2023-07-05 15:44:42 -0300

    Add files via upload

---
+++ Parada_19H/Parada_19H/js/codigo.js
@@ -0,0 +1,10 @@
+function muda_cor(){
+
+    var div = document.getElementById("box");
+    var cor = document.getElementById("i_color").value;
+
+    console.log(cor);
+
+    div.style.backgroundColor = cor;
+
+}
